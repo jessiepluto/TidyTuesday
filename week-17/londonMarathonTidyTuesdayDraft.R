17768c476ede8a5fbefb93fa451e81a2361cabfd
Jessie Pluto <jesspluto@gmail.com>
2023-04-25 18:35:31 -0400

    Add files via upload

---
+++ week-17/londonMarathonTidyTuesdayDraft.R
@@ -0,0 +1,93 @@
+librarian::shelf(tidyverse,ggplot2)
+
+tuesdata <- tidytuesdayR::tt_load('2023-04-25')
+tuesdata <- tidytuesdayR::tt_load(2023, week = 17)
+
+winners <- tuesdata$winners
+london_marathon <- tuesdata$london_marathon
+
+# trend of winners by Category over Year
+ggplot(winners, aes(x=Year)) + 
+  geom_line(aes(y=Time, col=Category)) + 
+  theme_bw()
+
+# facet grid of winners by Category over Year
+ggplot(winners, aes(Year, Time)) + 
+  geom_point(colour = "#88398A") + 
+  facet_grid(cols = vars(Category)) + 
+  theme_bw()
+
+# facet grid of winners by Category and Gender over Year
+winners %>% 
+  mutate(Gender = gsub('Wheelchair ','',Category),
+         Wheelchair = ifelse(nchar(Category) > 6,1,0)) %>% 
+  ggplot(aes(Year, Time)) + 
+  geom_point(colour = "#88398A") + 
+  facet_grid(cols = vars(Gender), rows = vars(Wheelchair), scales = "free_y") + 
+  theme_bw()
+
+
+athlete_df <- winners %>% 
+  filter(Category == "Women") %>% 
+  mutate(d = (min(Time) - Time)/60,
+         TimeRank = dense_rank(-d)) %>% 
+  group_by(Athlete) %>% 
+  mutate(minTime = min(Time)) %>% 
+  ungroup() %>% 
+  mutate(AthleteRank = dense_rank(minTime),
+         Athlete = fct_reorder(Athlete, -AthleteRank),
+         y = dense_rank(-AthleteRank)) %>%
+  select(-minTime)
+
+
+
+ggplot(athlete_df, aes(x=Athlete, y=d)) +
+  geom_segment(aes(x=Athlete, xend=Athlete, y=0, yend=d), color="#D3D3D3") +
+  geom_point(color="#88398A", size=5) +
+  coord_flip() +
+  scale_y_continuous(trans = "reverse") +
+  labs(
+    x = "",
+    y = "Seconds From Fastest Record",
+    title = "Fastest Winners of the Women's London Marathon"
+  ) +
+  annotate("text", label=athlete_df$TimeRank, x=athlete_df$y,y=athlete_df$d, color="white", size=3) +
+  theme_bw() +
+  theme(
+    panel.grid.major.y = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.border = element_blank(),
+    axis.text.y=element_text(hjust=1, color = "#562457", size = 10, margin = margin(0,0,10,20)),
+    axis.text.x=element_text(size = 10),
+    axis.ticks = element_blank(),
+    legend.position = 'none',
+    plot.margin = margin(10,10,10,10)  
+  )
+
+
+
+
+# winners plot from https://github.com/nrennie/LondonMarathon/blob/main/inst/plots.R
+ggplot(
+  data = winners %>%
+    # filter(Category == "Women") %>% 
+    group_by(Nationality) %>%
+    summarise(n = n()),
+  mapping = aes(
+    y = reorder(Nationality, n),
+    x = n)) +
+  geom_col(fill = "#88398A") +
+  geom_text(aes(label = n),
+            colour = "#88398A",
+            hjust = -1 ) +
+  labs(
+    x = "Number of winners",
+    title = "Nationality of London Marathon Winners") +
+  scale_x_continuous(limits = c(0, 50)) +
+  coord_cartesian(expand = FALSE) +
+  theme_minimal() +
+  theme(
+    axis.title.y = element_blank(),
+    plot.background = element_rect(fill = "white", colour = "white"),
+    panel.background = element_rect(fill = "white", colour = "white")
+  )
